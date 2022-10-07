0f504fe68cb4c2602d8c253b1c89382433ab34f6
Carlos Gioda <carlosgioda@192.168.1.13>
2022-10-07 00:16:56 -0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DirectivasComponent } from './components/directivas/directivas.component';
+import { DirectivaPersonalizadaDirective } from './directives/directiva-personalizada.directive';
+import { FormularioReactivoComponent } from './components/formulario-reactivo/formulario-reactivo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectivasComponent,
+    DirectivaPersonalizadaDirective,
+    FormularioReactivoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/formulario-reactivo/formulario-reactivo.component.ts
@@ -0,0 +1,28 @@
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario-reactivo',
+  templateUrl: './formulario-reactivo.component.html',
+  styleUrls: ['./formulario-reactivo.component.css']
+})
+export class FormularioReactivoComponent implements OnInit {
+  formularioRegistro: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.formularioRegistro = fb.group ({
+      nombre: new FormControl ('', [Validators.required, Validators.maxLength(12), Validators.minLength(3)]),
+      edad: new FormControl ('' , [Validators.maxLength(2)]),
+      correo: new FormControl ('', [Validators.email]),
+      contrasena: new FormControl ('' , [Validators.required,Validators.maxLength(15), Validators.minLength(8)])
+
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  agregarAlumno(){
+  }
+}
